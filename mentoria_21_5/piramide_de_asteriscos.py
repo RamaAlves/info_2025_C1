dfd83311f13af00a7207b9209460dab032a64b0b
Ramiro Alves <110850490+RamaAlves@users.noreply.github.com>
2025-05-21 23:27:07 -0300

    feat/resolviendo ejercicios con bucles y condicionales

---
+++ mentoria_21_5/piramide_de_asteriscos.py
@@ -0,0 +1,27 @@
+# Pirámide de asteriscos: Imprime una pirámide de asteriscos de 5 filas. 
+# La primera fila tendrá 1 asterisco, la segunda 2 con un espacio entre cada asterisco, y así sucesivamente. 
+# (Usa bucles anidados, si es necesario).
+
+#    * 
+#   * *
+#  * * *
+# * * * *
+#* * * * *
+
+#queda de tarea
+
+# Variante 2
+# ahora completar los espacios vacios intermedios con *
+
+#    *
+#   ***
+#  *****
+# *******
+#*********
+
+""" filas = 5
+
+for i in range(1, filas + 1):
+    print(" " * (filas - i), end="")
+    print("*" * (2 * i - 1))
+ """
